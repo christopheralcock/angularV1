f6d7a140fee05baaa3425d941d5fde208daff146
Christopher Alcock <christopher.alcock@gmail.com>
2015-09-10 09:31:50 +0100

    (0) test still failing

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -11,10 +11,12 @@ describe('GitHub profile finder', function() {
     expect(browser.getTitle()).toEqual('Github user search');
   });
 
+
   it('finds profiles', function() {
-    element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
-    element(by.className('btn')).click();
-    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
+    searchBox.sendKeys('spike');
+    searchButton.click();
+    var profiles = element.all(by.repeater('user searchResult.items'));
+    expect(profiles.get(1).getText()).toEqual('spike01');
   });
 
 });
